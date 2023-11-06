665aed728918663e53c219accccf0c43b5b45be9
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-11-06 15:32:21 +0900

    checkAutehntication,user api

--- routes/Users.js
+++ routes/Users.js
@@ -4,8 +4,18 @@ import {
     deleteUser,
     getUser,
     getAllUsers} from "../controllers/user.js"
+import { verifyToken, verifyUser } from "../utils/verifyToken.js"
 
 const router = express.Router();
+
+// Authentication 
+router.get("/checkAuthentication", verifyToken, (req, res, next)=>{
+    res.send("hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req, res, next)=>{
+    res.send("hello user, you are logged in and can update and delete")
+})
 
 //UPDATE ROOM API
 router.put("/:id", updateUser);
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken";
-import { createError } from "./error";
+import { createError } from "../utils/error.js"; //오류 미들웨어 .js추가해야 오류안생기더라 파일위치 표시도 확인!
+
+//verify 토큰 ; 인증,인가
+// 사용자 인증 미들웨어
+
+// 로그인을 통해 인증된 사용자만 방,예약에 대해 수정,삭제 생성 등의 권한이 있게끔 한다. next 콜백함수
 
 export const verifyToken = (req, res, next)=>{
     const token = req.cookies.access_token;
@@ -13,3 +18,13 @@ export const verifyToken = (req, res, next)=>{
         next()
     });
 };
+
+export const verifyUser = (req,res,next)=>{
+    verifyToken(req,res, ()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        } else{
+            return next(createError(403, "You are not authorized!"))
+        }
+    })
+}
