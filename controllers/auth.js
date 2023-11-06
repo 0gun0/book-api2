db73977783cd0799defa4a770cced9619427cdb2
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-11-06 09:39:59 +0900

    login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,17 @@
 import User from "../models/User.js"
+import bcrypt from "bcrypt"
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next)=>{
     try{
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash, //bcrypt 암호화
         })
 
         await newUser.save()
@@ -14,3 +20,20 @@ export const register = async (req, res, next)=>{
         next(err)
     }
 }
+
+// 로그인 API
+export const login = async (req, res, next) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) return next(createError(404, "User not found!"));
+  
+      const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password); // user 객체에서 비밀번호 가져오기
+      if (!isPasswordCorrect) return next(createError(400, "Wrong password"));
+  
+      // 필요없이 응답하는 요소 삭제 user._doc임
+      const {password, isAdmin, ...otherDetails } = user._doc;
+      res.status(200).json(otherDetails);
+    } catch (err) {
+      next(err);
+    }
+  };
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,19 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { register,login } from "../controllers/auth.js";
 
 const router = express.Router();
 
+//연결되었는지 확인차 
 router.get("/",(req,res)=>{
     res.send("hello! world. CONNECTED!")
 })
 
-//사용자 인증 미들웨어
+//사용자 인증,인가 미들웨어
 
+// 회원가입 API
 router.post("/register", register)
 
+// 로그인 API
+router.post("/login", login)
 
 export default router;
