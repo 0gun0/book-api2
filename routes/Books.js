1b7acb2be9c62db6d827f5bb7fdb3bbd5a31b3d8
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-11-08 15:29:59 +0900

    change create book

--- routes/Books.js
+++ routes/Books.js
@@ -8,54 +8,56 @@ const router = express.Router();
 
 // 예약 생성 API
 
-router.post('/',(req,res)=>{
-    const {roomId, userId, bookDate, bookTime, durationHours,startTime, endTime} = req.body
+router.post('/', (req, res) => {
+  const { roomId, userId, bookDate, bookTime, durationHours } = req.body;
 
-    //요청된 시간을 Date 객체로 파싱
-    const requestedTime = new Date(bookTime);
-    const requestedHour = requestedTime.getHours(); //utc시간으로 할때임.
+  // bookTime을 시작 시간과 종료 시간으로 분리
+  const [startTime, endTime] = bookTime.split(' - ');
 
-    // requestedTime.setHours(requestedTime.getHours() + 9); //UTC 시간을 한국시간으로 변환
-    
-    // const requestedHour = requestedTime.getHours(); //시간 추출 1시간 or 2시간
+  // startTime과 endTime을 Date 객체로 변환 (예: "17:00" -> "2023-11-08T17:00:00.000Z")
+  const startDateTime = new Date(`${bookDate}T${startTime}:00.000Z`);
+  const endDateTime = new Date(`${bookDate}T${endTime}:00.000Z`);
 
-    //9시부터 21시 사이에 1,2시간 단위로 예약
-    if (requestedHour >= 9 && requestedHour <= 21 && (requestedHour - 9) % durationHours === 0) {
-        // 중복 예약 확인 (이미 해당 시간대에 예약이 있는지 확인)
-        Book.find(
-          {
-            roomId: roomId,
-            bookTime: requestedTime
-          },
-          (err, existingBookings) => {
+  //9시부터 21시 사이에 1,2시간 단위로 예약
+  if (startDateTime.getHours() >= 9 && startDateTime.getHours() <= 21 && (startDateTime.getHours() - 9) % durationHours === 0) {
+    // 중복 예약 확인 (이미 해당 시간대에 예약이 있는지 확인)
+    Book.find(
+      {
+        roomId: roomId,
+        bookTime: startDateTime
+      },
+      (err, existingBookings) => {
+        if (err) {
+          res.status(500).json({ error: '예약 확인 중 오류가 발생했습니다.' });
+        } else if (existingBookings.length > 0) {
+          res.status(400).json({ error: '이미 해당 시간대에 예약이 있습니다.' });
+        } else {
+          // 새로운 예약을 생성
+          const book = new Book({
+            roomId,
+            userId,
+            bookDate,
+            bookTime: startDateTime.toISOString(), // 시작 시간을 ISO 문자열로 변환
+            durationHours
+          });
+
+          book.save((err) => {
             if (err) {
-              res.status(500).json({ error: '예약 확인 중 오류가 발생했습니다.' });
-            } else if (existingBookings.length > 0) {
-              res.status(400).json({ error: '이미 해당 시간대에 예약이 있습니다.' });
+              res.status(500).json({ error: '예약을 생성하는 동안 오류가 발생했습니다.' });
             } else {
-              // 새로운 예약을 생성
-              const book = new Book({
-                roomId,
-                userId,
-                bookTime: requestedTime,
-                durationHours
-              });
-
-              book.save((err) => {
-                if (err) {
-                  res.status(500).json({ error: '예약을 생성하는 동안 오류가 발생했습니다.' });
-                } else {
-                  res.status(201).json(book); //예약 성공시 book json을 찍어줌
-                }
-              });
+              res.status(201).json(book); // 예약 성공시 book json을 반환
             }
-          }
-        );
-      } else {
-        console.log(req.body)
-        res.status(400).json({ error: '선택한 시간대는 예약할 수 없거나 시간 간격이 올바르지 않습니다.' });
+          });
+        }
       }
-    });
+    );
+  } else {
+    console.log(req.body);
+    res.status(400).json({ error: '선택한 시간대는 예약할 수 없거나 시간 간격이 올바르지 않습니다.' });
+  }
+});
+
+
 
 // 예약 조회 API
 
